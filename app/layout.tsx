092ae0ecc265b04c2fbea8e65cd42be197b4a4be
Justin <justinlimweiyang@gmail.com>
2024-01-04 00:36:42 +0800

    troubleshooting logo missing during deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,10 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <body className={`${inter.className} antialiased text-4xl text-center`}>
-        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-      </body>
-    </div>
+    <body className={`${inter.className} antialiased text-4xl text-center`}>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </body>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 export default function Page() {
   return (
-    <div id='logo' className="mb-2 flex h-40 w-40 justify-start rounded-md bg-green-500 p-4">
-        <div className="w-20 text-white md:w-20 drop-shadow-[2.5px_1.2px_1.2px_rgba(0,0,0,0.8)]">
-            <h1>Jenny&apos;s Fruit Store</h1>
+    <div>
+        <div id='logo' className="mb-2 flex h-40 w-40 justify-start rounded-md bg-green-500 p-4">
+            <div className="w-20 text-white md:w-20 drop-shadow-[2.5px_1.2px_1.2px_rgba(0,0,0,0.8)]">
+                <h1>Jenny&apos;s Fruit Store</h1>
+            </div>
+        </div>
+
+        <div>
+            <p>hello world</p>
         </div>
     </div>
   );
