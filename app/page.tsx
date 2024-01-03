a1e92a9735e18dc129d2fc23b27422a8d06c884b
Justin <justinlimweiyang@gmail.com>
2024-01-04 00:42:30 +0800

    Fixed the logo disappearing issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 export default function Page() {
   return (
     <div>
-        <div id='logo' className="mb-2 flex h-40 w-40 justify-start rounded-md bg-green-500 p-4">
-            <div className="w-20 text-white md:w-20 drop-shadow-[2.5px_1.2px_1.2px_rgba(0,0,0,0.8)]">
+        <div id='logo' className="mb-2 flex h-30 w-60 justify-start rounded-md bg-green-500 p-4">
+            <div className="text-white h-20 md:w-60 drop-shadow-[2.5px_1.2px_1.2px_rgba(0,0,0,0.8)]">
                 <h1>Jenny&apos;s Fruit Store</h1>
             </div>
-        </div>
-
-        <div>
-            <p>hello world</p>
         </div>
     </div>
   );
