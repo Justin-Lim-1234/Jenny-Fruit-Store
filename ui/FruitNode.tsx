399359b6eb7953c77a90268f060498a95c1e1eae
Justin <justinlimweiyang@gmail.com>
2024-01-06 16:01:50 +0800

    Making changes to allow for total price calculation

--- ui/FruitNode.tsx
+++ ui/FruitNode.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useDebouncedCallback } from "use-debounce";
+import { useState } from "react";
 
 interface FruitNodeProps {
     id: number;
@@ -10,11 +11,16 @@ interface FruitNodeProps {
 }
 
 const FruitNode: React.FC<FruitNodeProps> = ({ id, name, price, qty }) => {
+    const [quantity, setQuantity] = useState(0);
 
     const handleInput = useDebouncedCallback((inputQty: string) => {
         if (inputQty) {
             const intQty = parseInt(inputQty);
+            setQuantity(intQty);
             console.log(`${name}: ${intQty}`);
+        }
+        else {
+            return 0;
         }
     }, 400);
 
@@ -22,7 +28,7 @@ const FruitNode: React.FC<FruitNodeProps> = ({ id, name, price, qty }) => {
         <div id="fnContainer">
             <div id="fnTitle">{name}</div>
             <label id="fnLabel">Enter Quantity:</label><br></br>
-            <input id="fnInput" onChange={(e) => handleInput(e.target.value)}></input>
+            <input id="fnInput" placeholder="0" onChange={(e) => handleInput(e.target.value)}></input>
         </div>
     );
 }
